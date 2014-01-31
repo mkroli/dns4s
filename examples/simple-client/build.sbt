575c94e0beea90443584d4a8e7e5b59a750114ce
Michael Krolikowski <mkroli@yahoo.de>
2014-01-31 22:50:16 +0100

    added client example

---
+++ examples/simple-client/build.sbt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2014 Michael Krolikowski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+name := "dns4s-example-simple-client"
+
+scalaVersion := "2.10.3"
+
+resolvers += "bintray" at "http://jcenter.bintray.com"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.2.3",
+  "com.github.mkroli" %% "dns4s-akka" % "0.2-SNAPSHOT")
---
+++ examples/simple-client/src/main/scala/com/github/mkroli/dns4s/examples/simple/client/DnsClient.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2014 Michael Krolikowski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.mkroli.dns4s.examples.simple.client
+
+import java.net.InetSocketAddress
+
+import scala.concurrent.duration.DurationInt
+
+import com.github.mkroli.dns4s.akka.Dns
+import com.github.mkroli.dns4s.dsl._
+
+import akka.actor.ActorSystem
+import akka.io.IO
+import akka.pattern.ask
+import akka.util.Timeout
+
+object DnsClient extends App {
+  implicit val system = ActorSystem("DnsServer")
+  implicit val timeout = Timeout(5 seconds)
+  import system.dispatcher
+
+  IO(Dns) ? Dns.DnsPacket(Query ~ Questions("google.de"), new InetSocketAddress("8.8.8.8", 53)) onSuccess {
+    case Response(Answers(answers)) =>
+      answers.collect {
+        case ARecord(arecord) => println(arecord.address.getHostAddress)
+      }
+      system.shutdown
+  }
+}
