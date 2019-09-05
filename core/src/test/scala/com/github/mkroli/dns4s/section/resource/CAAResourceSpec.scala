d69cef96cc6e1d13187ef07b0d397de9e646ff03
Bilal <bilal.m.fazlani@gmail.com>
2019-09-05 16:41:27 +0530

    Apply copyright suggestions from code review
    Co-Authored-By: Michael Krolikowski <mkroli@users.noreply.github.com>

--- core/src/test/scala/com/github/mkroli/dns4s/section/resource/CAAResourceSpec.scala
+++ core/src/test/scala/com/github/mkroli/dns4s/section/resource/CAAResourceSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Michael Krolikowski
+ * Copyright 2019 Bilal Fazlani
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
